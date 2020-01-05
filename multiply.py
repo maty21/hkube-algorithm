94116c643305aaa7704589f6850c576ee95fc80b
Cube <maty21@gmail.com>
2020-01-05 10:19:46 +0200

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -2,7 +2,7 @@ def start(args):
     print('algorithm: multiply start')
     input = args['input'][0]
     mul = args['input'][1]
-    print('algorithm: multiply END .    ')
+    print('algorithm: multiply END .     ')
     return input * mul
 
 
